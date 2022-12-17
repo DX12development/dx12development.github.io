ef18620c020bdb25513d053776445e3337a10810
DX12development <burritodavy@gmail.com>
2022-12-17 01:20:28 -0500

    s

--- communications.php
+++ communications.php
@@ -1,12 +1,8 @@
 <?php
 
-if(isset($_POST['user_name']))
+$data = $_REQUEST['user_message'];
 if(isset($_POST['user_message']))
 {
-$data=$_POST['user_name'];
-$fp = fopen('data.txt', 'a');
-fwrite($fp, $data);
-fclose($fp);
 $data=$_POST['user_message'];
 $fp = fopen('data.txt', 'a');
 fwrite($fp, $data);
